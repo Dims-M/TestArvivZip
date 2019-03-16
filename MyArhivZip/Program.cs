cc7192eebe13414352a41569fcb95aa93d26080c
Dims_M <dimsxm@yandex.ru>
2019-03-17 00:15:47 +0300

    Добавьте файлы проекта.

---
+++ MyArhivZip/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArhivZip
+{
+    class Program
+    {
+
+
+        //Загрузка выгрузка в нужныное место
+        //Создание папок загрузки выргузки
+        //Создание лога событий
+        //Сделать скрытым папку выгрузки
+        //Сделать архив скрытым.
+        // отправка архива на фтп 
+
+
+        // http://www.cyberforum.ru/csharp-beginners/thread92858.html
+
+        ////скрытый отримбцты
+        //System.IO.File.SetAttributes(@"H:\test.txt", System.IO.FileAttributes.Hidden);
+
+        ////только для чтения
+        //System.IO.File.SetAttributes(@"H:\test.txt", System.IO.FileAttributes.ReadOnly);
+
+        ////системный
+        //System.IO.File.SetAttributes(@"H:\test.txt", System.IO.FileAttributes.System);
+
+
+        //File.SetAttributes("download.exe", FileAttributes.Normal);
+        //File.Delete("download.exe");
+
+
+
+        static void Main(string[] args)
+        {
+
+   
+           MinuVibora(); //Запуск меню выбора
+          
+        }
+
+
+   
+        static void Stopee()
+        {
+            Console.ReadKey();
+        }
+
+
+      /// <summary>
+      /// Меню выбора режима работы архиватора
+      /// </summary>
+       static public void MinuVibora()
+        {
+            Console.WriteLine($"Здраствуйте. \n\t");
+            Console.WriteLine($"Выбирите нужое дествие: \n\t" +
+                "1) Если вы хотите архивировать обьект Введите 1 \n\t"+
+                "2)Если вы хотите разоархивировать обьект Введите 2 \n\t" +
+                "0) Выход из программы");
+
+            try
+            {
+
+                //Ждем выбора режима от пользователя
+                while (true)
+         {
+
+            var vibor = byte.Parse( Console.ReadLine());
+
+                if (vibor == 1)
+                {
+                    ZipFilesByMask.Checet();
+                    Console.WriteLine("Архив создан");
+                        break;
+                }
+
+                if (vibor == 2)
+                {
+                    ZipFilesByMask.RaspakovkaArhiva();
+                    
+                        break;
+                    }
+
+                if (vibor == 0)
+                {
+                  
+                    Console.WriteLine("Выход из прораммы");
+                        break;
+                    }
+
+                else
+                {
+                    Console.WriteLine("ВВедено не коректное значение...Проверте правильность ввода");
+                }
+
+            }
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка.Что то пошло не так ))))\t\n {ex}\t\n");
+            }
+
+            Console.ReadKey();
+
+        }
+
+
+        static void NameAdmin()
+        {
+
+            WindowsPrincipal pricipal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool hasAdministrativeRight = pricipal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (hasAdministrativeRight == false)
+            {
+                ProcessStartInfo processInfo = new ProcessStartInfo(); //создаем новый процесс
+                processInfo.Verb = "runas"; //в данном случае указываем, что процесс должен быть запущен с правами администратора
+                processInfo.FileName = "MinuVibora";   //указываем исполняемый файл (программу) для запуска
+
+                try
+                {
+                    Process.Start(processInfo); //пытаемся запустить процесс
+                }
+                catch (Win32Exception)
+                {
+                    //Ничего не делаем, потому что пользователь, возможно, нажал кнопку "Нет" в ответ на вопрос о запуске программы в окне предупреждения UAC (для Windows 7)
+                }
+               // Application.Exit(); //закрываем текущую копию программы (в любом случае, даже если пользователь отменил запуск с правами администратора в окне UAC)
+            }
+            else //имеем права администратора, значит, стартуем
+            {
+                MinuVibora();
+            }
+        }
+
+
+    }
+}
---
+++ MyArhivZip/ZipFilesByMask.cs
@@ -0,0 +1,106 @@
+﻿using ComponentAce.Compression.Archiver;
+using ComponentAce.Compression.ZipForge;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArhivZip
+{
+   public static class ZipFilesByMask
+    {
+
+        private static string pathArhivHille = @"c:\1\HiddenFolder";
+        private static string pathArhiv = @"c:\1";
+
+        /// <summary>
+        /// запуск архивирования
+        /// </summary>
+        public static void Checet()
+        {
+            // Create an instance of the ZipForge class
+            ZipForge archiver = new ZipForge();
+
+            try
+            {
+                archiver.FileName = @"C:\test.zip"; //Куда сохранить файл результата сжатия
+                archiver.OpenArchive(System.IO.FileMode.Create); //Настраиваем дллку на работу с новым архивом
+                archiver.BaseDir = @"C:\123"; //Папка где лежат все файлы для взятия
+               // archiver.AddFiles("*.exe"); //Берём все файлы с расширением exe
+               // archiver.AddFiles("*.dll");
+                //archiver.AddFiles("*.jpg");
+                archiver.AddFiles("*.*");
+
+                // archiver.BaseDir = @"D:\";
+                // archiver.AddFiles(@"d:\file.txt"); //Добавим один файл
+                // archiver.AddFiles(@"d:\Test"); //Запакуем ещё и папку
+                archiver.CloseArchive(); //Закрываем архив
+            }
+            //Ловим ошибки
+            catch (ArchiverException ae)
+            {
+                Console.WriteLine("Message: {0}\t Произошла ошибка: {1}",
+                                  ae.Message, ae.ErrorCode);
+                Console.ReadLine();
+            }
+            Console.WriteLine("Процес архивирования завершен....\t\n");
+        }
+
+        /// <summary>
+        /// Распаковка архива
+        /// </summary>
+        public static void RaspakovkaArhiva()
+        {
+            ZipForge archiver = new ZipForge();
+            NoVisiblePapka();
+            try
+            {
+                archiver.FileName = @"C:\test.zip"; //Необходимый файл
+                archiver.OpenArchive(System.IO.FileMode.Open); //Указываем что хотим сделать
+               // archiver.BaseDir = pathArhiv; //Папка куда распаковать
+                archiver.BaseDir = pathArhivHille; //Папка куда распаковать
+                //archiver.BaseDir = @"C:\1"; //Папка куда распаковать
+                // archiver.ExtractFiles("*.exe"); //Распаковываем *.exe
+                // archiver.ExtractFiles("*.dll"); //Распаковываем *.dll
+                archiver.ExtractFiles("*.*");
+
+                //archiver.BaseDir = @"D:\";
+               // archiver.ExtractFiles("*.txt"); //Распаковываем *.txt но можем и просто файл
+                archiver.ExtractFiles("test"); //Распаковываем папку
+                archiver.CloseArchive();
+
+                Console.WriteLine("Архив разохвивирован");
+            }
+            // Catch all exceptions of the ArchiverException type
+            catch (ArchiverException ae)
+            {
+                Console.WriteLine("Message: {0}\t Ошибка при архивации: {1}", ae.Message, ae.ErrorCode);
+                Console.ReadLine();
+            }
+        }
+
+
+        public static void NoVisiblePapka()
+        {
+            // string path = @"c:\1\HiddenFolder"; // путь где будет находится наша папка
+             string path = pathArhivHille; // путь где будет находится наша папка
+
+            //Проверка. Если данный путь существует. Если нет то создается
+            if (!Directory.Exists(path))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path);
+                di.Attributes = FileAttributes.Directory | FileAttributes.Hidden; //создание скрытой папка
+            }
+        }
+
+    }
+
+}
+
+
+    
+
+
+
